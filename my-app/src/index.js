0c0d0501f359886cab07a956e8c9a395ff5d8e56
Hiten Rathod <hitenrathod98@gmail.com>
2018-05-20 01:11:17 +0530

    Keep track of clicked squares

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,18 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square" onClick={() => alert('clicked ' + this.props.abc)}>
-        {this.props.abc}
+      <button className="square" onClick={ () => this.setState( {value: this.props.value } ) }>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      squares:Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square abc={i}/>;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => {
+        this.handleClick(i)
+      }}
+    />;
   }
 
   render() {
