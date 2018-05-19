96c7aff1b9921d0b3e0a3f0814670fc7b2b10ca1
Hiten Rathod <hitenrathod98@gmail.com>
2018-05-20 00:48:00 +0530

    Understand how this.prop works

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {this.props.value}
+        {this.props.abc}
       </button>
     );
   }
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square abc={i}/>;
   }
 
   render() {
