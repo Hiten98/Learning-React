f1a3f089282939a05ada3258dfe7b1e5a5cef4e7
Hiten Rathod <hitenrathod98@gmail.com>
2018-05-20 14:19:07 +0530

    prevent filling of same squares

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -19,12 +19,14 @@ class Board extends React.Component {
     };
   }
   handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = (this.state.xIsNext ? 'X' : 'O');
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    })
+    if (!this.state.squares[i]) {
+      const squares = this.state.squares.slice();
+      squares[i] = (this.state.xIsNext ? 'X' : 'O');
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      })
+    }
   }
   renderSquare(i) {
     return <Square
